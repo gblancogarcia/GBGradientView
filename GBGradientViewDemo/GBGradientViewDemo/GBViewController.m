fcec24689bae91815a9bcb6bc634f17766cd8db1
Gerardo Blanco <gblancogarcia@gmail.com>
2014-05-18 12:03:25 +0200

    Added gradient orientation button.

--- GBGradientViewDemo/GBGradientViewDemo/GBViewController.m
+++ GBGradientViewDemo/GBGradientViewDemo/GBViewController.m
@@ -13,7 +13,6 @@
 
 #import <QuartzCore/QuartzCore.h>
 
-const CGFloat ColorAnimationDuration = 1.0f;
 const CGFloat ColorAnimationDelay = 1.0f;
 
 @interface GBViewController ()
@@ -21,8 +20,16 @@ const CGFloat ColorAnimationDelay = 1.0f;
 @property (nonatomic, strong) GBGradientView *backgroundView;
 @property (nonatomic, strong) UIColor *highColor;
 @property (nonatomic, strong) UIColor *lowColor;
-@property (nonatomic, strong) GBRoundBorderedButton *button;
+@property (nonatomic, strong) GBRoundBorderedButton *playButton;
+@property (nonatomic, strong) GBRoundBorderedButton *animationDurationButton;
+@property (nonatomic, strong) UIButton *orientationButton;
 @property (nonatomic, strong) NSMutableArray *colors;
+@property (nonatomic) CGFloat animationDuration;
+@property (nonatomic) GBGradientViewOrientation orientation;
+@property(nonatomic) CGAffineTransform verticalOrientationTransform;
+@property(nonatomic) CGAffineTransform horizontalOrientationTransform;
+
+@property (nonatomic) BOOL keepColors;
 
 @end
 
@@ -48,10 +55,12 @@ const CGFloat ColorAnimationDelay = 1.0f;
 
 - (NSMutableArray *)colors
 {
-    if (_colors) {
+    if (_colors && (self.keepColors == NO)) {
         self.highColor = [UIColor nextColor:self.highColor];
         self.lowColor = [UIColor nextColor:self.lowColor];
     }
+    
+    self.keepColors = NO;
     
     _colors = [NSMutableArray array];
     
@@ -80,82 +89,27 @@ const CGFloat ColorAnimationDelay = 1.0f;
 
 - (void)setUp
 {
+    self.keepColors = false;
+    self.animationDuration = 3.0f;
+    
     [self setupBackgroundView];
     
-    CGRect frame = CGRectMake(0.0f, 0.0f, 200.0f, 48.0f);
-    
-    self.button = [[GBRoundBorderedButton alloc] initWithFrame:frame];
-
-    self.button.center = self.view.center;
-    self.button.color = [UIColor whiteColor];
-    self.button.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    [self.button setTitle:@"Start" forState:UIControlStateNormal];
-    [self.button addTarget:self action:@selector(startOrStop) forControlEvents:UIControlEventTouchUpInside];
-
-    [self.view addSubview:self.button];
-    
-    NSLayoutConstraint *width = [NSLayoutConstraint constraintWithItem:self.button
-                                                               attribute:NSLayoutAttributeWidth
-                                                               relatedBy:NSLayoutRelationEqual
-                                                                  toItem:nil
-                                                               attribute:NSLayoutAttributeNotAnAttribute
-                                                              multiplier:1.0f
-                                                                constant:200.0f];
-    [self.view addConstraint:width];
-    
-    NSLayoutConstraint *height = [NSLayoutConstraint constraintWithItem:self.button
-                                                             attribute:NSLayoutAttributeHeight
-                                                             relatedBy:NSLayoutRelationEqual
-                                                                toItem:nil
-                                                             attribute:NSLayoutAttributeNotAnAttribute
-                                                            multiplier:1.0f
-                                                              constant:48.0f];
-    [self.view addConstraint:height];
-    
-    NSLayoutConstraint *centerX = [NSLayoutConstraint constraintWithItem:self.button
-                                                                  attribute:NSLayoutAttributeCenterX
-                                                                  relatedBy:NSLayoutRelationEqual
-                                                                     toItem:self.view
-                                                                  attribute:NSLayoutAttributeCenterX
-                                                                 multiplier:1.0f
-                                                                   constant:0.0f];
-    [self.view addConstraint:centerX];
-    
-    NSLayoutConstraint *centerY = [NSLayoutConstraint constraintWithItem:self.button
-                                                               attribute:NSLayoutAttributeCenterY
-                                                               relatedBy:NSLayoutRelationEqual
-                                                                  toItem:self.view
-                                                               attribute:NSLayoutAttributeCenterY
-                                                              multiplier:1.0f
-                                                                constant:0.0f];
-    [self.view addConstraint:centerY];
-}
-
--(void)startOrStop
-{
-    if ([self.button.titleLabel.text isEqualToString:@"Start"]) {
-        [self.button setTitle:@"Stop" forState:UIControlStateNormal];
-        [self.backgroundView startAnimating];
-    } else {
-        [self.backgroundView stopAnimating];
-        self.button.enabled = NO;
-    }
-}
-
-- (void)updateUI
-{
-
+    [self setUpPlayButton];
+    [self setUpAnimationDurationButton];
+    [self setUpOrientationButton];
 }
 
 - (void)setupBackgroundView
 {
     if (!self.backgroundView) {
+        self.orientation = GBGradientViewOrientationVertical;
         self.backgroundView = [[GBGradientView alloc] initWithFrame:self.view.bounds
-                                                        orientation:GBGradientViewOrientationVertical];
+                                                        orientation:self.orientation];
         
         self.backgroundView.delegate = self;
         self.backgroundView.translatesAutoresizingMaskIntoConstraints = NO;
+        self.backgroundView.animationDuration = self.animationDuration;
+        self.backgroundView.animationDelay = ColorAnimationDelay;
         
         [self.view addSubview:self.backgroundView];
         
@@ -173,6 +127,201 @@ const CGFloat ColorAnimationDelay = 1.0f;
     }
 }
 
+- (void)setUpPlayButton
+{
+    CGRect frame = CGRectMake(0.0f, 0.0f, 128.0f, 44.0f);
+    
+    self.playButton = [[GBRoundBorderedButton alloc] initWithFrame:frame];
+    
+    self.playButton.center = self.view.center;
+    self.playButton.color = [UIColor whiteColor];
+    self.playButton.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.playButton setTitle:@"Start" forState:UIControlStateNormal];
+    [self.playButton addTarget:self action:@selector(startOrStop) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:self.playButton];
+    
+    NSLayoutConstraint *width = [NSLayoutConstraint constraintWithItem:self.playButton
+                                                             attribute:NSLayoutAttributeWidth
+                                                             relatedBy:NSLayoutRelationEqual
+                                                                toItem:nil
+                                                             attribute:NSLayoutAttributeNotAnAttribute
+                                                            multiplier:1.0f
+                                                              constant:128.0f];
+    [self.view addConstraint:width];
+    
+    NSLayoutConstraint *height = [NSLayoutConstraint constraintWithItem:self.playButton
+                                                              attribute:NSLayoutAttributeHeight
+                                                              relatedBy:NSLayoutRelationEqual
+                                                                 toItem:nil
+                                                              attribute:NSLayoutAttributeNotAnAttribute
+                                                             multiplier:1.0f
+                                                               constant:44.0f];
+    [self.view addConstraint:height];
+    
+    NSLayoutConstraint *centerX = [NSLayoutConstraint constraintWithItem:self.playButton
+                                                               attribute:NSLayoutAttributeCenterX
+                                                               relatedBy:NSLayoutRelationEqual
+                                                                  toItem:self.view
+                                                               attribute:NSLayoutAttributeCenterX
+                                                              multiplier:1.0f
+                                                                constant:0.0f];
+    [self.view addConstraint:centerX];
+    
+    NSLayoutConstraint *centerY = [NSLayoutConstraint constraintWithItem:self.playButton
+                                                               attribute:NSLayoutAttributeCenterY
+                                                               relatedBy:NSLayoutRelationEqual
+                                                                  toItem:self.view
+                                                               attribute:NSLayoutAttributeCenterY
+                                                              multiplier:1.0f
+                                                                constant:8.0f];
+    
+    [self.view addConstraint:centerY];
+}
+
+- (void)setUpAnimationDurationButton
+{
+    CGRect frame = CGRectMake(0.0f, 0.0f, 44.0f, 44.0f);
+    
+    self.animationDurationButton = [[GBRoundBorderedButton alloc] initWithFrame:frame];
+    
+    NSString *title = [NSString stringWithFormat:@"%.0f", self.animationDuration];
+    
+    [self.animationDurationButton setTitle:title forState:UIControlStateNormal];
+    [self.animationDurationButton addTarget:self action:@selector(changeAnimationDuration) forControlEvents:UIControlEventTouchUpInside];
+    
+    self.animationDurationButton.color = [UIColor whiteColor];
+    
+    [self.view addSubview:self.animationDurationButton];
+    
+    self.animationDurationButton.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    NSLayoutConstraint *width = [NSLayoutConstraint constraintWithItem:self.animationDurationButton
+                                                             attribute:NSLayoutAttributeWidth
+                                                             relatedBy:NSLayoutRelationEqual
+                                                                toItem:nil
+                                                             attribute:NSLayoutAttributeNotAnAttribute
+                                                            multiplier:1.0f
+                                                              constant:44.0f];
+    [self.view addConstraint:width];
+    
+    NSLayoutConstraint *height = [NSLayoutConstraint constraintWithItem:self.animationDurationButton
+                                                              attribute:NSLayoutAttributeHeight
+                                                              relatedBy:NSLayoutRelationEqual
+                                                                 toItem:nil
+                                                              attribute:NSLayoutAttributeNotAnAttribute
+                                                             multiplier:1.0f
+                                                               constant:44.0f];
+    [self.view addConstraint:height];
+    
+    NSLayoutConstraint *centerX = [NSLayoutConstraint constraintWithItem:self.animationDurationButton
+                                                               attribute:NSLayoutAttributeCenterX
+                                                               relatedBy:NSLayoutRelationEqual
+                                                                  toItem:self.view
+                                                               attribute:NSLayoutAttributeCenterX
+                                                              multiplier:1.0f
+                                                                constant:0.0f];
+    
+    [self.view addConstraint:centerX];
+    
+    NSLayoutConstraint *bottom = [NSLayoutConstraint constraintWithItem:self.animationDurationButton
+                                                              attribute:NSLayoutAttributeBottom
+                                                              relatedBy:NSLayoutRelationEqual
+                                                                 toItem:self.playButton
+                                                              attribute:NSLayoutAttributeTop
+                                                             multiplier:1.0f
+                                                               constant:-24.0f];
+    
+    [self.view addConstraint:bottom];
+}
+
+- (void)setUpOrientationButton
+{
+    self.orientationButton = [[UIButton alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 64.0f, 64.0f)];
+    
+    [self.orientationButton setImage:[UIImage imageNamed:@"OrientationButton"] forState:UIControlStateNormal];
+    [self.orientationButton setImage:[UIImage imageNamed:@"OrientationButtonHighlighted"] forState:UIControlStateHighlighted];
+    
+    [self.orientationButton addTarget:self
+                        action:@selector(switchGradientViewOrientation)
+              forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:self.orientationButton];
+    
+    [self.orientationButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    NSLayoutConstraint *centerX = [NSLayoutConstraint constraintWithItem:self.orientationButton
+                                                               attribute:NSLayoutAttributeCenterX
+                                                               relatedBy:NSLayoutRelationEqual
+                                                                  toItem:self.view
+                                                               attribute:NSLayoutAttributeCenterX
+                                                              multiplier:1.0f
+                                                                constant:0.0f];
+    
+    [self.view addConstraint:centerX];
+    
+    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:self.orientationButton
+                                                           attribute:NSLayoutAttributeTop
+                                                           relatedBy:NSLayoutRelationEqual
+                                                              toItem:self.playButton
+                                                           attribute:NSLayoutAttributeBottom
+                                                          multiplier:1.0f
+                                                            constant:12.0f];
+    
+    [self.view addConstraint:top];
+    
+    self.verticalOrientationTransform = self.orientationButton.transform;
+    self.horizontalOrientationTransform = CGAffineTransformRotate(self.orientationButton.transform, -M_PI_2);
+}
+
+- (void)updateUI
+{
+    
+}
+
+- (void)changeAnimationDuration
+{
+    self.animationDuration =  self.animationDuration + 1.0f;
+    
+    if (self.animationDuration > 5.0f) {
+        self.animationDuration = 1.0f;
+    }
+    
+    self.backgroundView.animationDuration = self.animationDuration;
+    
+    NSString *title = [NSString stringWithFormat:@"%.0f", self.animationDuration];
+    [self.animationDurationButton setTitle:title forState:UIControlStateNormal];
+}
+
+- (void)startOrStop
+{
+    if ([self.playButton.titleLabel.text isEqualToString:@"Start"]) {
+        [self.playButton setTitle:@"Stop" forState:UIControlStateNormal];
+        [self.backgroundView startAnimating];
+    } else {
+        [self.backgroundView stopAnimating];
+        self.playButton.enabled = NO;
+        self.animationDurationButton.enabled = NO;
+        self.orientationButton.enabled = NO;
+    }
+}
+
+- (void)switchGradientViewOrientation
+{
+    self.keepColors = YES;
+    
+    if (self.orientation == GBGradientViewOrientationHorizontal) {
+        self.orientation = GBGradientViewOrientationVertical;
+        self.orientationButton.transform = self.verticalOrientationTransform;
+    } else {
+        self.orientation = GBGradientViewOrientationHorizontal;
+        self.orientationButton.transform = self.horizontalOrientationTransform;
+    }
+    
+    self.backgroundView.orientation = self.orientation;
+}
+
 - (NSArray *)gradientColorsForGradientView:(GBGradientView *)gradientView
 {
     return [self.colors mutableCopy];
@@ -181,8 +330,10 @@ const CGFloat ColorAnimationDelay = 1.0f;
 - (void)gradientViewAnimationDidStop:(GBGradientView *)gradientView
 {
     if (self.backgroundView.isAnimating == NO) {
-        self.button.enabled = YES;
-        [self.button setTitle:@"Start" forState:UIControlStateNormal];
+        self.playButton.enabled = YES;
+        self.animationDurationButton.enabled = YES;
+        self.orientationButton.enabled = YES;
+        [self.playButton setTitle:@"Start" forState:UIControlStateNormal];
     }
 }
 
